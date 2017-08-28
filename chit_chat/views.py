dfb3c0e24b1d84d1f2169419204c0026141d9c4e
Aditya Kumar <aditya.kumar774@gmail.com>
2017-08-29 01:44:06 +0530

    views created

--- chit_chat/views.py
+++ chit_chat/views.py
@@ -2,5 +2,47 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
-# Create your views here.
+from chat_app import settings
+from .models import chat
+
+def Login(request):
+    next = request.GET.get('next', '/home/')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(next)
+            else:
+                return HttpResponse("Account is not active.")
+        else:
+            return HttpResponseRedirect(settings.LOGIN_URL)
+    return render(request, "chit_chat/login.html", {'next': next})
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+def Home(request):
+    c = Chat.objects.all()
+    return render(request, "chit_chat/home.html", {'home': 'active', 'chat': c})
+
+def Post(request):
+    if request.method == 'POST':
+        msg = request.POST.get('msgbox', None)
+        c = Chat(user=request.user, message=msg)
+        if msg != '':
+            c.save()
+        return JsonResponse({'msg': msg, 'user': c.user.username})
+    else:
+        return HttpResponse('Request must be POST.')
+
+def Messages(request):
+    c = Chat.objects.all()
+    return render(request, 'chit_chat/messages.html', {'chat': c})
